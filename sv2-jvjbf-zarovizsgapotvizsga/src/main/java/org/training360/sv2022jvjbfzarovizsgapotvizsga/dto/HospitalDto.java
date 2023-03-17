03a4ce0b6d366f7aaad375605f808e98fa3c0bed
codriv <combodriver4@yahoo.at>
2023-03-17 18:12:49 +0100

    List<PatientDto>

--- sv2-jvjbf-zarovizsgapotvizsga/src/main/java/org/training360/sv2022jvjbfzarovizsgapotvizsga/dto/HospitalDto.java
+++ sv2-jvjbf-zarovizsgapotvizsga/src/main/java/org/training360/sv2022jvjbfzarovizsgapotvizsga/dto/HospitalDto.java
@@ -3,7 +3,6 @@ package org.training360.sv2022jvjbfzarovizsgapotvizsga.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.training360.sv2022jvjbfzarovizsgapotvizsga.model.Patient;
 
 import java.util.List;
 
